f16b5ee74c47539e11cdf9e3e0cb30439d3eae9e
Joshua Ho <ho.joshua4@gmail.com>
2016-02-14 18:12:19 -0800

    Webpack properly generated via webpack.config - Gulpfile is broken.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 var app = express();
 var PORT = 5000;
 
-app.use(express.static( __dirname + '/app'))
+app.use(express.static( __dirname + '/build'))
 .listen(PORT, () => {
   console.log('Server is running on port ' + PORT)
 })
