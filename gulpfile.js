2edfd13c711adf7c283c5f34719b8cd01c9cd62b
Ardian Ajvazi <ardian_46@hotmail.com>
2016-02-14 17:18:36 -0800

    stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
+const preset = require('babel-preset-react');
 const babel = require('babel-loader');
 
 gulp.task('html:dev', () => {
@@ -16,17 +17,6 @@ gulp.task('webpack:dev', () => {
     }))
     .pipe(gulp.dest('build/'));
 });
-
-gulp.task('webpack:test', () => {
-  gulp.src(__dirname + '/test/test_entry.js')
-    .pipe(webpack({
-      output: {
-        filename: 'test_bundle.js'
-      }
-    }))
-    .pipe(gulp.dest('test/'));
-});
-
 
 gulp.task('build:dev', ['webpack:dev', 'html:dev']);
 gulp.task('default', ['build:dev']);
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = 5000;
 
 app.use(express.static( __dirname + '/app'))
 .listen(PORT, () => {
