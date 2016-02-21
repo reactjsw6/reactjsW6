65a9ca60d685832fdb2e91f44bb2bd5e3fece1ac
Ardian Ajvazi <ardian_46@hotmail.com>
2016-02-20 21:07:44 -0800

    delete and edit

--- app/js/client.js
+++ app/js/client.js
@@ -6,44 +6,56 @@ var URI = "http://localhost:3000/api/bears";
 var BearsApp = React.createClass({
   getInitialState: function() {
     this.getBears();
-    return {
-      bears: []
+    return { bears: [] };
+  },
+
+  deleteBear: function(bearId) {
+    return () => {
+      var something = this.state.bears.filter(function(bear) {
+        if(bear._id === bearId) {
+          return false;
+        }else {
+          return true;
+        }
+      });
+      this.setState({bears: something});
     }
   },
 
+  // editBear: function(id) {
+  //   return () => {
+  //     var something = this.state.bears.map(function(bear) {
+  //       if(id === bear._id) {
+  //         return bear;
+  //       }
+  //     });
+  //     this.setState({bears: something});
+  //   }
+  // },
+
   getBears: function() {
     this.serverRequest = $.get(URI)
-      .then( (data) => {
-        data.forEach(function(bear) {
-          bear.editing = false;
-        });
-        this.setState( {bears:data} );
-      })
-  },
-
-  componentWillUnmount: function() {
-    this.serverRequest.abort();
-  },
-
-  //Render display data
-  render: function() {
-    //Creating dynamic data
-    var displayData = this.state.bears.map(function (bears) {
-      return (
-        <div className="bearItem" key={bears._id}>
-          {bears.name} the bear loves {bears.fishPreference}
-        </div>
-      )
+    .then((data) => {
+      data.forEach(function(bear) {
+        bear.editing = false;
+      });
+      this.setState({bears:data});
     });
-    //Final return
-    //displayData is defined above
-    return (
-      <div className="bearsList">
-        {displayData}
-      </div>
-    )
+  },
+
+  render: function() {
+    return (<ul> {this.state.bears.map((bear) => {
+      return <li key={bears._id}> {bear.name}, {bear.flavor}, {bear.fishPreference} <button onClick={this.deleteBear(bear._id)}>Delete</button>
+      <form key={bears._id}>
+      < input type = "text" name = "bearName" placeholder = "Bear Name"/>
+      < input type = "text" name = "bearFlavor" placeholder = "Bear Flavor"/>
+      < button type = "submit" > Edit < /button >
+      </form>
+      </li>
+
+    })
+  }</ul>)
   }
+});
 
-});//End of BearsApp
-
-DOM.render(<BearsApp /> , document.getElementById('bear') );
+DOM.render(<BearsApp /> , document.getElementById('bears'));
--- server.js
+++ server.js
@@ -1,16 +1,3 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const app = module.exports = exports = express();
 
-//Our database woop woop
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/bears_app');
-
-const bearsRouter = require( __dirname + '/routes/bear_routes');
-
-var PORT = 5000;
-
-app.use(bearsRouter);
-app.use(express.static( __dirname + '/build'));
-app.listen(PORT, () => {
-  console.log('Server is running on port ' + PORT);
-});
+express().use(express.static(__dirname + '/build')).listen(5000, () => console.log('Server up on PORT 5000'));
