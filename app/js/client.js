315591aa1c7aa8e41f29908069a3f362fe602d2c
Gene Troy <genetroy@hotmail.com>
2016-02-22 00:55:03 -0800

    POST and PUT work

--- app/js/client.js
+++ app/js/client.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const DOM = require('react-dom');
 
-var URI = "http://localhost:3000/api/bears";
+var URI = "http://localhost:5000/api/bears";
 
 var BearsApp = React.createClass({
   //Start the server, run these things
@@ -12,40 +12,82 @@ var BearsApp = React.createClass({
 
   getBears: function(){
     $.get(URI).then( (data) => {
-        data.forEach( (bear) => {
-          bear.editing = false;
-        });
-        this.setState( {bears: data} );
+      data.forEach( (bear) => {
+        bear.editing = false;
+      });
+      this.setState( {bears: data} );
     })
   },
 
   deleteBear: function(bearId) {
     return () => {
       var holder = this.state.bears.filter(function(bear) {
-        if (bear._id === bearId._id) return false;
-        return true;
+        return bear._id !== bearId._id;
       });
-      //
+
       this.setState({ bears: holder });
+
       $.ajax({
         url: URI + '/' + bearId,
         type: 'DELETE'
-        }).then( (data) => {
-          console.log(data)
-        });
+      }).then( (data) => {
+        console.log(data)
+      });
     };
   },
 
-  // editBear: function(id) {
-  //   return () => {
-  //     var something = this.state.bears.map(function(bear) {
-  //       if(id === bear._id) {
-  //         return bear;
-  //       }
-  //     });
-  //     this.setState({bears: something});
-  //   }
-  // },
+  findDatBear: function (id) {
+    console.log("findDatBear", id);
+    return this.state.bears.filter(function(bear) {
+      return bear._id === id;
+    }).shift();
+  },
+
+  handleUpdate: function(id) {
+    var that = this;
+    return function (e) {
+      var bear = that.findDatBear(id);
+      console.log("handleUpdate", bear);
+
+      if (bear) {
+        bear[e.target.name] = e.target.value;
+        that.setState(that.state);
+      } else {
+        newBear = that.state.newBear || {};
+        newBear[e.target.name] = e.target.value;
+        that.setState({ newBear: newBear });
+      }
+    }
+  },
+
+  updateBear: function(id) {
+    var that = this;
+    return function (e) {
+      var bear = that.findDatBear(id);
+
+      console.log("updateBear", bear);
+
+      $.ajax({
+        url: URI + "/" + id,
+        method: "PUT",
+        contentType: "application/json",
+        data: JSON.stringify(bear),
+      }).then(function () {
+        console.log("updateBear", "FUCK YEAH");
+      });
+    };
+  },
+
+  newBear: function (e) {
+    $.ajax({
+      url: URI,
+      method: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(this.state.newBear),
+    }).then(function () {
+      console.log("NEW BEAR", "FUCK YEAH");
+    });
+  },
 
   render: function() {
     // .map takes each object in data and uses the following specs to create HTML
@@ -54,70 +96,44 @@ var BearsApp = React.createClass({
         //HTML
           <tr key={bear._id}>
             <td>
-              <input type="text" defaultValue={bear.name}></input>
+              <input name="name" type="text" defaultValue={bear.name} onChange={this.handleUpdate(bear._id)}></input>
             </td>
             <td>
-              <input type="text" defaultValue={bear.fishPreference}></input>
+              <input name="fishPreference" type="text" defaultValue={bear.fishPreference} onChange={this.handleUpdate(bear._id)}></input>
             </td>
             <td>
               <button onClick={this.deleteBear(bear._id)} className="btn btn-danger glyphicon glyphicon-remove"></button>
             </td>
             <td>
-              <button className="btn btn-basic glyphicon glyphicon-pencil"></button>
+              <button onClick={this.updateBear(bear._id)} className="btn btn-basic glyphicon glyphicon-pencil"></button>
             </td>
           </tr>
       )
     })
     return (
       //HTML
-      <table className="table table-striped table-hover bearList">
-        <thead>
-          <tr>
-            <td>Name</td><td>Fish Preference</td><td>Delete</td><td>Update</td>
-          </tr>
-        </thead>
-        <tbody>
-          {bearObjects}
-        </tbody>
-      </table>
+      <div>
+        <table className="table table-striped table-hover bearList">
+          <thead>
+            <tr>
+              <td>Name</td><td>Fish Preference</td><td>Delete</td><td>Update</td>
+            </tr>
+          </thead>
+          <tbody>
+            {bearObjects}
+          </tbody>
+        </table>
+        <div>
+          <ul>
+          <li>Name<input name="name" type="text" onChange={this.handleUpdate("new")}></input></li>
+          <li>Fish Preference<input name="fishPreference" type="text" onChange={this.handleUpdate("new")}></input></li>
+          <li><button onClick={this.newBear} className="btn btn-basic glyphicon glyphicon-pencil"></button></li>
+          </ul>
+        </div>
+      </div>
     )
   }
 
 });
-
-    // return (
-    //   <h2> This is being rendered from Client </h2>
-    //   <ul>
-    //     {this.state.bears.map( (bear) => {
-    //       return
-    //         <li key={bears._id}> {bear.name}, {bear.flavor}, {bear.fishPreference}
-    //           <button onClick={this.deleteBear(bear._id)}>Delete</button>
-    //           <form key={bears._id}>
-    //             <input type = "text" name = "bearName" placeholder = "Bear Name"/>
-    //             <input type = "text" name = "bearFlavor" placeholder = "Bear Flavor"/>
-    //           <button type = "submit">Edit</button >
-    //           </form>
-    //         </li>
-    //       })}
-    //   </ul>
-    // )
-
-
-
-
-// render: function() {
-//     var commentNodes = this.props.data.map(function(comment) {
-//       return (
-//         <Comment author={comment.author} key={comment.id}>
-//           {comment.text}
-//         </Comment>
-//       );
-//     });
-//     return (
-//       <div className="commentList">
-//         {commentNodes}
-//       </div>
-//     );
-//   }
 
 DOM.render(<BearsApp /> , document.getElementById('bears'));
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = module.exports = exports = express();
+const mongoose = require('mongoose');
+
+const bearsRouter = require(__dirname + '/lib/bear_routes_3000');
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/bears_app_dev');
 
 const PORT = 5000;
 
 app.use(
   express.static(__dirname + '/build')
 );
+
+app.use('/api', bearsRouter);
+
 app.listen( PORT , () => {
   console.log('Server up on PORT 5000')
 });
